bd0d97bbb27d35b990f58522a237804793373569
Jenna Castillo <jennacodes101@gmail.com>
2022-07-09 11:29:54 +0200

    created a basic rails program

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list tasks (read_all)
+  root to: 'tasks#index'
+  get 'index', to: 'tasks#index'
+  # place new first because it has more constrains than show
+  get 'index/new', to: 'tasks#new', as: 'new'
+  # read indvidual
+  get 'index/:id', to: 'tasks#show', as: 'show'
+  # create
+  post 'index/new', to: 'tasks#create'
+  # update
+  get '/index/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch '/index/:id/edit', to: 'tasks#update'
+  # delete
+  delete '/index/:id', to: 'tasks#destroy', as: 'destroy'
 end
